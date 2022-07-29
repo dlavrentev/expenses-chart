96e310ff8380876931ae0e1d569b5e385f9c4b2b
dlavrentev <dimitrilavrentev96@gmail.com>
2022-07-29 10:19:04 +0200

    updated spending css and js

--- src/Components/Spending.js
+++ src/Components/Spending.js
@@ -4,6 +4,36 @@ const Spending = () => {
   return (
     <div className="spending-container">
       <h1>Spending - Last 7 days</h1>
+      <div className="chart-container">
+        <div className="bar">
+          <div></div>
+          <p>mon</p>
+        </div>
+        <div className="bar">
+          <div></div>
+          <p>tue</p>
+        </div>
+        <div className="bar">
+          <div></div>
+          <p>wed</p>
+        </div>
+        <div className="bar">
+          <div></div>
+          <p>thu</p>
+        </div>
+        <div className="bar">
+          <div></div>
+          <p>fri</p>
+        </div>
+        <div className="bar">
+          <div></div>
+          <p>sat</p>
+        </div>
+        <div className="bar">
+          <div></div>
+          <p>sun</p>
+        </div>
+      </div>
       <div className="line"></div>
       <div className="spending-bottom">
         <div className="spending-bottom-l">
