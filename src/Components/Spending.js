842fa5ea5d8266864be52911ed1317213481c579
dlavrentev <dimitrilavrentev96@gmail.com>
2022-08-01 13:00:23 +0200

    deleted spending js

--- src/Components/Spending.js
+++ src/Components/Spending.js
@@ -6,8 +6,9 @@ const Spending = () => {
   const [isHovering, setisHovering] = useState(false);
 
   const handleMouseEnter = (e) => {
+    console.log(e)
     setisHovering(false);
-    console.log(target.e)
+    
   };
 
   const handleMouseLeave = () => {
@@ -18,30 +19,31 @@ const Spending = () => {
     <div className="spending-container">
       <h1>Spending - Last 7 days</h1>
       <div className="chart-container">
-        {data.map(({ id, day, amount }) => {
-          return (
-            <div className="bar" key={id}>
-              <div
-                
-                className={`detail`}
-                id={`${isHovering ? `hidden${id}` : `visible${id}`}`}
-              >
-                ${amount}
-              </div>
-              <div
-                key={id}
-                className={`bars bar-${id}`}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                style={{
-                  height: `${(data[id - 1].amount / 52.36) * 150}px`,
-                }}
-              ></div>
-              <p>{day}</p>
-            </div>
-          );
-        })}
-      </div>
+  <div className="bar">
+    <div
+      className={`detail`}
+      id={`visible1`}
+    >
+      $50.02
+    </div>
+    {data.map(({ id, day, amount }) => {
+      return (
+        <div className="bars">
+          <div
+            key={id}
+            className={`bar-${id}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            style={{
+              height: `${(data[id - 1].amount / 52.36) * 150}px`,
+            }}
+          ></div>
+          <p>{day}</p>
+        </div>
+      );
+    })}
+  </div>
+</div>;
       <div className="line"></div>
       <div className="spending-bottom">
         <div className="spending-bottom-l">
