8ba6fc10fad4b1441eb4b218c06b45d951e1d3c4
dlavrentev <dimitrilavrentev96@gmail.com>
2022-08-01 14:17:37 +0200

    updated

--- src/Components/Spending.js
+++ src/Components/Spending.js
@@ -1,22 +1,46 @@
-import React from "react";
-// import data from "./../Data/data.json";
+import React, { useState } from "react";
+import data from "./../Data/data.json";
 
 const Spending = () => {
-  // const [isHovering, setisHovering] = useState(false);
 
-  // const handleMouseEnter = () => {
-  //   setisHovering(false);
-  // };
+  const [isHovering, setisHovering] = useState(false);
 
-  // const handleMouseLeave = () => {
-  //   setisHovering(true);
-  // };
+  const handleMouseEnter = (e) => {
+    setisHovering(false);
+    console.log(e._targetInst.alternate.key)
+  };
+
+  const handleMouseLeave = () => {
+    setisHovering(true);
+  };
 
   return (
     <div className="spending-container">
       <h1>Spending - Last 7 days</h1>
       <div className="chart-container">
-        
+        {data.map(({ id, day, amount }) => {
+          return (
+            <div className="bar" key={id}>
+              <div
+                
+                className={`detail`}
+                id={`${isHovering ? `hidden${id}` : `visible${id}`}`}
+              >
+                ${amount}
+              </div>
+              <div
+                key={id}
+                className={`bars bar-${id}`}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                style={{
+                  height: `${(data[id - 1].amount / 52.36) * 150}px`,
+                }}
+              ></div>
+              <p>{day}</p>
+            </div>
+          );
+        })}
       </div>
       <div className="line"></div>
       <div className="spending-bottom">
